8f21a421a39eb45e93e2881d49b29e6c700a894b
Rajput Vivek <167712861+Rajput-Vivek-16@users.noreply.github.com>
2025-07-20 20:27:19 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,18 +9,18 @@ const Header = () => {
           <img src={process.env.PUBLIC_URL + "/Vivek.png"} width="250" alt="Vivek Rajput" />
         </div>
         <div className="header-text">
-          <h1>Hi There,</h1>
+      
           <h2>I'm <span className="highlight">Vivek</span> Rajput</h2>
           <p className="intro-text">
-            I Am Into <strong>Chemical Engineering</strong> & <strong>Web Development</strong>
+             <strong>Junior Undergraduate</strong> pursuing a major in <strong>Chemical Engineering</strong> with a minor in <strong>Computer Science and Engineering</strong> at <strong>IIT Gandhinagar</strong>.
           </p>
           
           <div className="academic-details">
             <h3>Academic Details</h3>
             <ul>
               <li><strong>B.Tech, IIT Gandhinagar (2023-Present):</strong> 8.5 CPI</li>
-              <li><strong>Class XII, Riverdale Academy (2022-2023):</strong> 73.5%</li>
-              <li><strong>Class X, Good Shepherd Mission School (2020-2021):</strong> 92%</li>
+              <li><strong>Class XII, Riverdale Academy, Surat (2022-2023):</strong> 73.5%</li>
+              <li><strong>Class X, Good Shepherd High School, Surat (2020-2021):</strong> 92%</li>
             </ul>
           </div>
 
